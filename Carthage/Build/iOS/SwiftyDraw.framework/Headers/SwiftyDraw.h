cad884cb93f61e10e7ed131f000b0ca049793f37
Andrew Kharchyshyn <haawaplus@gmail.com>
2017-05-14 18:41:24 +0300

    Carthage support

---
+++ Carthage/Build/iOS/SwiftyDraw.framework/Headers/SwiftyDraw.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyDraw.h
+//  SwiftyDraw
+//
+//  Created by Andriy K. on 5/14/17.
+//  Copyright © 2017 Walzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyDraw.
+FOUNDATION_EXPORT double SwiftyDrawVersionNumber;
+
+//! Project version string for SwiftyDraw.
+FOUNDATION_EXPORT const unsigned char SwiftyDrawVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDraw/PublicHeader.h>
+
+
